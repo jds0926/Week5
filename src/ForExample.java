08a1059baa41c780b843f1f57e18776dfef2a49e
JDS <202430328@email.daelim.ac.kr>
2024-10-02 16:51:58 +0900

    Initial commit

---
+++ src/ForExample.java
@@ -0,0 +1,10 @@
+public class ForExample {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("i = " + i);
+            for (int j = 0; j < i; j++) {
+                System.out.println("j = " + j);
+            }
+        }
+    }
+}
